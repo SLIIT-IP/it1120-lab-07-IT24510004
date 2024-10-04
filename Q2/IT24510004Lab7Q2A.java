9bc9d3281fd9944cc0dae8755cc63cf16b961510
IT24510004 <it24510004@my.sliit.lk>
2024-10-04 05:57:05 +0530

    IT24510004Lab7Q2A

---
+++ Q2/IT24510004Lab7Q2A.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class IT24510004Lab7Q2A
+{
+    public static void main(String[] args)
+    { 
+        
+        for(int x=1;x<5;x++)
+           {
+                System.out.println("$ $ $ $ $");
+            }
+            
+        
+    }
+}
